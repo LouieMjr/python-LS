a254453532b38faf5e07dc15a9e3d78686f0e5c3
Louie Mendez <lmendezjr024@gmail.com>
2024-12-22 17:43:54 -0500

    welcome to calculator

---
+++ py101-basics/calculator/calculator.py
@@ -0,0 +1,8 @@
+# Ask the user for the first number.
+# Ask the user for the second number.
+# Ask the user for an operation to perform.
+# Perform the operation on the two numbers.
+# Print the result to the terminal.
+
+def welcome_to_calculator():
+    print('Welcome to Calculator')
