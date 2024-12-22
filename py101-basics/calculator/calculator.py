25c4d6660afb56f32eb5c957992dce7d6b972c08
Louie Mendez <lmendezjr024@gmail.com>
2024-12-22 18:25:09 -0500

    getting input from user and handling error on invalid number input

--- py101-basics/calculator/calculator.py
+++ py101-basics/calculator/calculator.py
@@ -4,5 +4,25 @@
 # Perform the operation on the two numbers.
 # Print the result to the terminal.
 
+
+from os import system
+
+def get_number_from_user(called):
+    first_or_second = 'First' if called == 1 else 'Second'
+    try:
+        num = float(input(f'Enter the {first_or_second} number: '))
+        return num
+    except:
+        system('clear')
+        print('Not a valid input. Make sure input is a number!')
+        return get_number_from_user(called)
+
+
 def welcome_to_calculator():
     print('Welcome to Calculator')
+    called = 1
+    print(get_number_from_user(called))
+    called += 1
+    print(get_number_from_user(called))
+
+welcome_to_calculator()
