39f4206b9b63b5491e9faec30fba7e3d07a56e9a
Louie Mendez <lmendezjr024@gmail.com>
2024-12-23 15:58:52 -0500

    checking for a valid operation, performing operation on two number inputs, using lambda functions as a value in a dict. Calculator appears to be working correct. Will test more

--- py101-basics/calculator/calculator.py
+++ py101-basics/calculator/calculator.py
@@ -18,22 +18,60 @@ def get_number_from_user(called):
         return num
     except:
         system('clear')
-        print(messages['invalid'])
+        print(messages['invalid_number'])
         return get_number_from_user(called)
 
 def get_operation_from_user():
-    print(messages['operation'])
-    operation = input(messages['choices'])
-    print(operation)
+    print(messages['which_operation'])
+    user_choice = input(messages['choices'])
+    return valid_operation(user_choice)
 
+def valid_operation(operation):
+    options = ['a', 's', 'm', 'd']
+    operation = operation.lower()[0]
 
+    if operation in options:
+        return operation
+    else:
+        system('clear')
+        print(messages['invalid_operation'])
+        return get_operation_from_user() 
+
+def perform_operation_on_numbers(num1, num2, operation):
+    system('clear')
+    options = {
+        "a": ['+', lambda a, b : a + b],
+        "s": ['-', lambda a, b : a - b],
+        "m": ['x', lambda a, b : a * b],
+        "d": ['÷', lambda a, b : a / b],
+    }
+
+    return f'{num1} {options[operation][0]} {num2} = {options[operation][1](num1, num2)}'
+    
+def clear_console_print_inputs(num1, num2 = None):
+    system('clear')
+    inputs = [num1, num2]
+
+    if inputs[1] == None:
+        print(f'You previously entered: {num1}')
+    else:
+        inputs[0] = str(inputs[0])
+        inputs[1] = str(inputs[1])
+        numbers = ' and '.join(inputs)
+        print(f'You previously entered: {numbers}')
 
 def welcome_to_calculator():
     print(messages['welcome'])
-    called = 1
-    num1 = get_number_from_user(called)
-    called += 1
-    num2 = get_number_from_user(called)
-    operation = get_operation_from_user()
 
-welcome_to_calculator()
+    called = 1
+    input_number1 = int(get_number_from_user(called))
+    clear_console_print_inputs(input_number1)
+
+    called += 1
+    input_number2 = int(get_number_from_user(called))
+    clear_console_print_inputs(input_number1, input_number2)
+
+    user_choice = get_operation_from_user()
+    return perform_operation_on_numbers(input_number1, input_number2, user_choice)
+
+print(welcome_to_calculator())
