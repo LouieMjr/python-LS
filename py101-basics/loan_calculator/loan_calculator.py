7d633b46517a1aec55a99915334bf9750f67e064
Louie Mendez <lmendezjr024@gmail.com>
2024-12-27 12:50:44 -0500

    print welcome message char by char. Ask user for loan amount after a delay

---
+++ py101-basics/loan_calculator/loan_calculator.py
@@ -0,0 +1,30 @@
+from time import sleep
+import json
+import os
+
+def open_json():
+    with open('./messages.json', 'r') as file:
+        return json.load(file)
+
+MESSAGES = open_json()
+
+def get_loan_amount_from_user():
+    loan_amount = input(MESSAGES['ask_for_loan_amount'])
+    return loan_amount
+
+def delay(time):
+    sleep(time)
+
+def print_char_one_by_one(message):
+    for char in message:
+        print(char, end='', flush=True)
+        delay(0.03)
+
+    print('\n')
+
+def start_loan_calc():
+    print_char_one_by_one(MESSAGES['welcome'])
+    delay(2)
+    print(get_loan_amount_from_user())
+
+start_loan_calc()
