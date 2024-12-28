d2c1ddb9416237d4adcacb76c9b3b9212da6216f
Louie Mendez <lmendezjr024@gmail.com>
2024-12-27 20:39:02 -0500

    adding print char line by line func to every message

--- py101-basics/loan_calculator/loan_calculator.py
+++ py101-basics/loan_calculator/loan_calculator.py
@@ -9,7 +9,8 @@ def open_json():
 MESSAGES = open_json()
 
 def get_loan_amount_from_user():
-    loan_amount = input(MESSAGES['ask_for_loan_amount'])
+    print_char_one_by_one(MESSAGES['ask_for_loan_amount'])
+    loan_amount = input()
     return loan_amount
 
 def delay(time):
@@ -20,7 +21,7 @@ def print_char_one_by_one(message):
         print(char, end='', flush=True)
         delay(0.03)
 
-    print('\n')
+    # print('\n')
 
 def start_loan_calc():
     print_char_one_by_one(MESSAGES['welcome'])
