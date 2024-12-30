165c133150f5600c77226c13270589064b1954bb
Louie Mendez <lmendezjr024@gmail.com>
2024-12-30 09:46:26 -0500

    added function to get loan term in years and converting it to months. Building out monthly payment function

--- py101-basics/loan_calculator/loan_calculator.py
+++ py101-basics/loan_calculator/loan_calculator.py
@@ -18,8 +18,21 @@ def get_annual_percentage_rate():
     APR = float(input())
     return APR
 
-# def get_loan_duration():
-    
+def calculate_monthly_interest_rate():
+    APR = get_annual_percentage_rate()
+    monthly_interest_rate = APR / 12
+    return monthly_interest_rate
+
+def get_loan_duration_years():
+    print_char_horizontally(MESSAGES['ask_for_loan_duration'])
+    loan_term = int(input())
+    return loan_term
+
+def convert_loan_term_to_months():
+    loan_term_years = get_loan_duration_years()
+    months_in_one_year = 12
+    loan_duration_in_months = months_in_one_year * loan_term_years
+    return loan_duration_in_months
 
 def delay(time):
     sleep(time)
@@ -29,10 +42,14 @@ def print_char_horizontally(message):
         print(char, end='', flush=True)
         delay(0.03)
 
+def calculate_monthly_payment():
+    loan_amount = get_loan_amount_from_user()
+    monthly_interest_rate = calculate_monthly_interest_rate()
+    loan_term_months = convert_loan_term_to_months()
+
 def start_loan_calc():
     print_char_horizontally(MESSAGES['welcome'])
     delay(1.5)
-    loan_amount = get_loan_amount_from_user()
-    print(get_annual_percentage_rate())
+    calculate_monthly_payment()
 
 start_loan_calc()
