e1ef5482ed4af742ac635efa9b7cb2ef6dd90e7d
Louie Mendez <lmendezjr024@gmail.com>
2024-12-30 09:19:57 -0500

    function to get annual percentage rate

--- py101-basics/loan_calculator/loan_calculator.py
+++ py101-basics/loan_calculator/loan_calculator.py
@@ -9,23 +9,30 @@ def open_json():
 MESSAGES = open_json()
 
 def get_loan_amount_from_user():
-    print_char_one_by_one(MESSAGES['ask_for_loan_amount'])
-    loan_amount = input()
+    print_char_horizontally(MESSAGES['ask_for_loan_amount'])
+    loan_amount = float(input())
     return loan_amount
+
+def get_annual_percentage_rate():
+    print_char_horizontally(MESSAGES['ask_for_APR'])
+    APR = float(input())
+    return APR
+
+# def get_loan_duration():
+    
 
 def delay(time):
     sleep(time)
 
-def print_char_one_by_one(message):
+def print_char_horizontally(message):
     for char in message:
         print(char, end='', flush=True)
         delay(0.03)
 
-    # print('\n')
-
 def start_loan_calc():
-    print_char_one_by_one(MESSAGES['welcome'])
-    delay(2)
-    print(get_loan_amount_from_user())
+    print_char_horizontally(MESSAGES['welcome'])
+    delay(1.5)
+    loan_amount = get_loan_amount_from_user()
+    print(get_annual_percentage_rate())
 
 start_loan_calc()
