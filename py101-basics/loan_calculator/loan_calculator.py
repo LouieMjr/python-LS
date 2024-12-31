38735f73112b0eb237712606d680b89e08690a02
Louie Mendez <lmendezjr024@gmail.com>
2024-12-31 09:39:17 -0500

    added input validation, shortened variable names

--- py101-basics/loan_calculator/loan_calculator.py
+++ py101-basics/loan_calculator/loan_calculator.py
@@ -8,31 +8,45 @@ def open_json():
 
 MESSAGES = open_json()
 
-def get_loan_amount_from_user():
-    print_char_horizontally(MESSAGES['ask_for_loan_amount'])
-    loan_amount = int(input())
+def get_loan_amount_from_user(print_char):
+    print_char(MESSAGES['ask_for_loan_amount'])
+    try:
+        loan_amount = int(input())
+    except ValueError:
+        system('clear')
+        print_char(MESSAGES['invalid_input'])
+        return get_loan_amount_from_user(print_char)
     system('clear')
     return loan_amount
 
-def get_annual_percentage_rate():
-    print_char_horizontally(MESSAGES['ask_for_APR'])
-    APR = (float(input()) / 100)
+def get_annual_percentage_rate(print_char):
+    print_char(MESSAGES['ask_for_APR'])
+    try:
+        APR = (float(input()) / 100)
+    except ValueError:
+        system('clear')
+        print_char(MESSAGES['invalid_input'])
+        return get_annual_percentage_rate(print_char)
     return APR
 
-def calculate_monthly_interest_rate():
-    APR = get_annual_percentage_rate()
+def calculate_monthly_interest_rate(print_char):
+    APR = get_annual_percentage_rate(print_char)
     system('clear')
     monthly_interest_rate = (APR / 12)
-
     return [monthly_interest_rate, APR]
 
-def get_loan_duration_years():
-    print_char_horizontally(MESSAGES['ask_for_loan_duration'])
-    loan_term = int(input())
+def get_loan_duration_years(print_char):
+    print_char(MESSAGES['ask_for_loan_duration'])
+    try:
+        loan_term = int(input())
+    except ValueError:
+        system('clear')
+        print_char(MESSAGES['invalid_input'])
+        return get_loan_duration_years(print_char)
     return loan_term
 
-def convert_loan_term_to_months():
-    loan_term_years = get_loan_duration_years()
+def convert_loan_term_to_months(print_char):
+    loan_term_years = get_loan_duration_years(print_char)
     months_in_one_year = 12
     loan_duration_in_months = months_in_one_year * loan_term_years
     return loan_duration_in_months
@@ -45,28 +59,29 @@ def print_char_horizontally(message):
         print(char, end='', flush=True)
         delay(0.02)
 
-def calculate_monthly_payment():
-    principle = get_loan_amount_from_user()
-    monthly_rate, APR = calculate_monthly_interest_rate()
-    loan_term = convert_loan_term_to_months()
+def calculate_monthly_payment(print_char):
+    principle = get_loan_amount_from_user(print_char)
+    monthly_rate, APR = calculate_monthly_interest_rate(print_char)
+    loan_term = convert_loan_term_to_months(print_char)
 
     monthly_payment = principle * (monthly_rate / (1 - (1 + monthly_rate) ** (-loan_term)))
-
     monthly_payment = f'{monthly_payment:.2f}'
 
-    borrow_amount = f'{MESSAGES['borrow_amount']}{principle:,}.\n\n'
+    borrow_amount_msg = f'{MESSAGES['borrow_amount']}{principle:,}.\n\n'
     annual_per_rate = f'{MESSAGES['APR']} {APR}%.\n'
     monthly_int_rate = f'{MESSAGES['monthly_interest_rate']} {monthly_rate:.5f}%.\n'
-    loan_duration = f'You want to pay this off in {loan_term} months.\n\n'
-    monthly_payment_message = f'After taking all of this into account,\n{MESSAGES['monthly_payment']}{monthly_payment}'
+    loan_duration = f'{MESSAGES['loan_duration']} {loan_term} months.\n\n'
+    monthly_payment_msg = f'{MESSAGES['monthly_payment']}{monthly_payment}'
 
     system('clear')
 
-    print_char_horizontally(f'{borrow_amount}{annual_per_rate}{monthly_int_rate}{loan_duration}{monthly_payment_message}\n')
+    print_char(f'{borrow_amount_msg}{annual_per_rate}{monthly_int_rate}{loan_duration}')
+    delay(0.8)
+    print_char(f'{monthly_payment_msg}\n')
 
 def start_loan_calc():
     print_char_horizontally(MESSAGES['welcome'])
     delay(0.8)
-    calculate_monthly_payment()
+    calculate_monthly_payment(print_char_horizontally)
 
 start_loan_calc()
