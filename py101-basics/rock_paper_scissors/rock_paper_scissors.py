00dc7dae87cc0780e09eda464725df3629876875
Louie Mendez <lmendezjr024@gmail.com>
2025-01-02 14:13:22 -0500

    validating user input, building computer choice next

--- py101-basics/rock_paper_scissors/rock_paper_scissors.py
+++ py101-basics/rock_paper_scissors/rock_paper_scissors.py
@@ -9,6 +9,8 @@
 # scissors:
 # scissors > paper
 from time import sleep
+from os import system
+from random import choice
 
 def delay(time):
     sleep(time)
@@ -18,20 +20,41 @@ def print_with_typing_effect(message):
         print(char, end='', flush=True)
         delay(0.02)
 
-def get_user_choice():
-    print_with_typing_effect('Make your selection: Rock(r), Paper(p), Scissors(s): ')
-    user_choice = input().upper()
+def get_user_choice(msg = 'Make your selection: Rock(r), Paper(p), Scissors(s): '):
+    print_with_typing_effect(msg)
+    user_choice = input()
 
-    # validate input
+    user_choice = valid_choice(user_choice)
+    while user_choice == False:
+        return get_user_choice('\nRock(r), Paper(p), Scissors(s): ')
+
+    return user_choice
+
+
+def valid_choice(user_pick):
+    user_pick = user_pick.upper()
+
     choices = {
         "R": "Rock",
         "P": "Paper",
         "S": "Scissors"
     }
 
-    if user_choice[0] in choices:
-        user_choice = choices[user_choice[0]]
-        print_with_typing_effect('You picked: ' + user_choice + '\n')
-    # validate input could be put in separate function
+    try:
+        if user_pick[0] in choices:
+            system('clear')
+            user_pick = choices[user_pick]
+            print_with_typing_effect('You picked: ' + user_pick + '\n')
+            return user_pick
+        else:
+            system('clear')
+            print_with_typing_effect('This selection is not a valid one.\nPlease pick one of the choices we suggest!\n')
+            return False
+    except IndexError:
+        system('clear')
+        print_with_typing_effect('This selection is not a valid one.\nPlease pick one of the choices we suggest!\n')
+        return False
 
-get_user_choice()
+def computer_choice():
+    
+computer_choice()
