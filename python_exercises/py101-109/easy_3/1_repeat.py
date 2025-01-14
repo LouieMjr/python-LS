b3121f4fd07686e230398390f6739e9846138719
Louie Mendez <lmendezjr024@gmail.com>
2025-01-14 15:08:16 -0500

    save

--- python_exercises/py101-109/easy_3/1_repeat.py
+++ python_exercises/py101-109/easy_3/1_repeat.py
@@ -1,8 +1,7 @@
 # Write a function that takes two arguments, a string and a positive integer, then prints the string as many times as the integer indicates.
-
+ 
 def repeat(string_to_repeat, times):
     for _ in range(times):
         print(string_to_repeat)
-
 
 repeat('Hello', 3)
