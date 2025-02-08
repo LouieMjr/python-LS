dea24f1960a3b66a3ae3892100216f7af0b4c27d
Louie Mendez <lmendezjr024@gmail.com>
2025-02-07 20:48:23 -0500

    added a custom print function for a typing effect

--- py110-intermediate/projects/tictactoe/tic_tac_toe.py
+++ py110-intermediate/projects/tictactoe/tic_tac_toe.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 from random import randrange, choice
+from time import sleep
 
 position_on_board = ('1', '2', '3', '4', '5', '6', '7', '8', '9')
 
@@ -15,6 +16,13 @@ x_and_y_axis_on_board = {
     9: [2, 2],
 }
 
+def print_with_typing_effect(message):
+    for char in message:
+        print(char, end='', flush=True)
+        sleep(0.02)
+
+PRINT = print_with_typing_effect
+
 def create_board():
     board = []
 
@@ -22,7 +30,6 @@ def create_board():
         board.append(['-' for _ in range(3)])
 
     return board
-
 
 BOARD = create_board()
 
@@ -32,34 +39,35 @@ def display_board(board):
 
 def computer_start_character(user_pick):
     if user_pick == 'X':
-        print('Computer picked: O')
+        PRINT('Computer picked: O\n\n')
         return 'O'
     else:
-        print('Computer picked: X')
+        PRINT('Computer picked: X\n\n')
         return 'X'
 
 def user_start_character():
-    pick = input('\nTo begin select a character: X or O: ').upper()
-    print(f'You picked: {pick}')
+    PRINT("\n\nWhen you're ready, select a character: X or O: ")
+    pick = input().upper()
+    PRINT(f'\nYou picked: {pick}\n')
     return pick
 
 # def user_selection_on_board(board):
 
 def display_rules():
-    rules = "Tic Tac Toe is a 2-player game played on a 3x3 grid called the board.\nEach player takes a turn and marks a square on the board. The first player\nto get 3 squares in a row -- horizontally, vertically, or diagonally -- wins.\nIf all 9 squares are filled and neither player has 3 in a row, the game ends in a tie."
+    rules = "\nTic Tac Toe is a 2-player game played on a 3x3 grid called the board.\nEach player takes a turn and marks a square on the board. The first player\nto get 3 squares in a row -- horizontally, vertically, or diagonally -- wins.\nIf all 9 squares are filled and neither player has 3 in a row, the game ends in a tie."
 
     winning_board1 = f'{['X', 'o', 'x']}-{['o', 'o', 'X']}-{['X', 'X', 'X']}\n'
     winning_board2 = f'{['X', 'o', 'o']}-{['o', 'X', 'X']}-{['o', 'o', 'x']}\n'
     winning_board3 = f'{['X', 'x', 'o']}-{['X', 'o', 'o']}-{['x', 'o', 'o']}'
 
-    print(rules, '\n')
-    print(f'Here are some examples of winning boards\n\n{winning_board1}{winning_board2}{winning_board3}')
+    PRINT(rules)
+    PRINT(f'\n\nHere are some examples of winning boards.\n\n{winning_board1}{winning_board2}{winning_board3}')
 
 def pick_square():
-    print('Make a selection on the board\nHere are your options:\n')
+    PRINT('\nMake a selection on the board\nHere are your options:\n')
     board_options = f'{[1, 2, 3]}\n{[4, 5, 6]}\n{[7, 8, 9]}\n'
-    print(board_options) 
-    position = int(input('Enter the number on the board that\ncorrelates to the position you want to select: '))
+    PRINT(board_options) 
+    position = int(input('Enter the number on the board that correlates to the position you want to select: '))
     return position
 
 # def is_board_position_open(list_of_numbers):
@@ -94,25 +102,22 @@ def game_logic():
             update_board(x_and_y, key_to_delete)
             display_board(BOARD)
 
-            print('yes')
         else:
             print('no')
 
-
-game_logic()
-
+# game_logic()
 
 def start_tic_tac_toe():
-    print('Lets play some Tic Tac Toe!')
-    print("Here are the rules before we begin.\n")
+    PRINT('Lets play some Tic Tac Toe!\n')
+    PRINT("Here are the rules before we begin.\n")
     display_rules()
 
     player_pick = user_start_character()
-    computer_pick = computer_start_character(player_pick)
+    computer_start_character(player_pick)
 
     display_board(BOARD)
+    game_logic()
 
 
-
-# start_tic_tac_toe()
+start_tic_tac_toe()
 
