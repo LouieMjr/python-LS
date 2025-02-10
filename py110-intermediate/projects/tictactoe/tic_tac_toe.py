9193e1d9ed867a001a206ae56d96ca283b564b5f
Louie Mendez <lmendezjr024@gmail.com>
2025-02-10 17:09:13 -0500

    able to validate inputs for both character select and selecting position on game board

--- py110-intermediate/projects/tictactoe/tic_tac_toe.py
+++ py110-intermediate/projects/tictactoe/tic_tac_toe.py
@@ -1,10 +1,15 @@
-from pprint import pprint
-from random import randrange, choice
+from random import choice
 from time import sleep
+from os import system
+import json
+from typing import is_typeddict
 
-position_on_board = ('1', '2', '3', '4', '5', '6', '7', '8', '9')
+with open('./TTT_messages.json') as file:
+    MESSAGES = json.load(file)
 
-x_and_y_axis_on_board = {
+board_options = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+INTERNAL_BOARD = {
     1: [0, 0],
     2: [0, 1],
     3: [0, 2],
@@ -16,10 +21,11 @@ x_and_y_axis_on_board = {
     9: [2, 2],
 }
 
-def print_with_typing_effect(message):
+
+def print_with_typing_effect(message, time = 0.02):
     for char in message:
         print(char, end='', flush=True)
-        sleep(0.02)
+        sleep(time)
 
 PRINT = print_with_typing_effect
 
@@ -31,9 +37,9 @@ def create_board():
 
     return board
 
-BOARD = create_board()
+DISPLAY_BOARD = create_board()
 
-def display_board(board):
+def print_board(board):
     for row in board:
         print(row)
 
@@ -45,67 +51,96 @@ def computer_start_character(user_pick):
         PRINT('Computer picked: X\n\n')
         return 'X'
 
-def user_start_character():
-    PRINT("\n\nWhen you're ready, select a character: X or O: ")
+def user_start_character(msg = MESSAGES['character_select']):
+    PRINT(msg)
     pick = input().upper()
-    PRINT(f'\nYou picked: {pick}\n')
-    return pick
 
-# def user_selection_on_board(board):
+    if validate_user_character(pick):
+        system('clear')
+        PRINT(f'\nYou picked: {pick}\n')
+        return pick
+    else:
+        return user_start_character(MESSAGES['invalid_character'])
+
+def validate_user_character(input):
+    if input != 'X' and input != 'O':
+        return False
+    return True
 
 def display_rules():
-    rules = "\nTic Tac Toe is a 2-player game played on a 3x3 grid called the board.\nEach player takes a turn and marks a square on the board. The first player\nto get 3 squares in a row -- horizontally, vertically, or diagonally -- wins.\nIf all 9 squares are filled and neither player has 3 in a row, the game ends in a tie."
 
     winning_board1 = f'{['X', 'o', 'x']}-{['o', 'o', 'X']}-{['X', 'X', 'X']}\n'
     winning_board2 = f'{['X', 'o', 'o']}-{['o', 'X', 'X']}-{['o', 'o', 'x']}\n'
     winning_board3 = f'{['X', 'x', 'o']}-{['X', 'o', 'o']}-{['x', 'o', 'o']}'
 
-    PRINT(rules)
-    PRINT(f'\n\nHere are some examples of winning boards.\n\n{winning_board1}{winning_board2}{winning_board3}')
+    rules = MESSAGES['rules']
+    # PRINT(rules)
 
-def pick_square():
-    PRINT('\nMake a selection on the board\nHere are your options:\n')
-    board_options = f'{[1, 2, 3]}\n{[4, 5, 6]}\n{[7, 8, 9]}\n'
-    PRINT(board_options) 
-    position = int(input('Enter the number on the board that correlates to the position you want to select: '))
-    return position
+    win_ex_msg = MESSAGES['win_example_msg']
+    # PRINT(f'{win_ex_msg}{winning_board1}{winning_board2}{winning_board3}')
 
-# def is_board_position_open(list_of_numbers):
-#     x, y = list_of_numbers
-#     board_position = BOARD[x][y]
-#     if board_position == '-':
-#         return True
-#     return False
+def user_picks_square():
+    if board_options == [[1, 2, 3], [4, 5, 6], [7, 8, 9]]:
+        PRINT(MESSAGES['available_board_options'])
+
+    row1, row2, row3 = board_options
+    display_board_options = f'\n{row1}\n{row2}\n{row3}\n\n'
+    PRINT(display_board_options)
+
+    try:
+        position = int(input(MESSAGES['select_position']))
+
+        if is_valid_board_position(position):
+            system('clear')
+            return [position, board_options]
+        else:
+            PRINT(MESSAGES['unavailable'])
+            return user_picks_square()
+
+    except (ValueError, TypeError):
+        PRINT(MESSAGES['unavailable'])
+        return user_picks_square()
+
+
+def is_valid_board_position(number):
+    valid_number = number in [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    if isinstance(number, int) and valid_number:
+        return True
+    return False
 
 
 def random_computer_choice(remaining_options):
     pick = choice(remaining_options)
-    return [pick, x_and_y_axis_on_board[pick]]
+    return [pick, INTERNAL_BOARD[pick]]
 
-def update_board(list_of_numbers, key, player = 'computer'):
+def update_board(list_of_numbers, key, options_remaining, player = 'computer'):
     x, y = list_of_numbers
-    BOARD[x][y] = 'O' if player == 'computer' else 'X'
-    del x_and_y_axis_on_board[key]
+    character = 'O' if player == 'computer' else 'X'
+    DISPLAY_BOARD[x][y] = character
+    del INTERNAL_BOARD[key]
+    options_remaining[x][y] = '-'
+
+
 
 def game_logic():
-    while x_and_y_axis_on_board != {}:
+    while INTERNAL_BOARD != {}:
 
-        selection = pick_square()
+        square, board_options = user_picks_square()
 
-        if x_and_y_axis_on_board.get(selection) is not None:
-            user_x_and_y = x_and_y_axis_on_board[selection]
-            update_board(user_x_and_y, selection, 'user')
-
-            positions_remaining = list(x_and_y_axis_on_board.keys())
-
-            key_to_delete, x_and_y = random_computer_choice(positions_remaining)
-            update_board(x_and_y, key_to_delete)
-            display_board(BOARD)
+        if INTERNAL_BOARD.get(square) is None:
+            PRINT(f'Position {square} is unavailable. Try again.\n')
 
         else:
-            print('no')
+            is_winner(DISPLAY_BOARD)
+            user_x_and_y = INTERNAL_BOARD[square]
+            update_board(user_x_and_y, square, board_options, 'user')
 
-# game_logic()
+            positions_remaining = list(INTERNAL_BOARD.keys())
+
+            computer_square, x_and_y = random_computer_choice(positions_remaining)
+            update_board(x_and_y, computer_square, board_options)
+            print_board(DISPLAY_BOARD)
 
 def start_tic_tac_toe():
     PRINT('Lets play some Tic Tac Toe!\n')
@@ -115,7 +150,6 @@ def start_tic_tac_toe():
     player_pick = user_start_character()
     computer_start_character(player_pick)
 
-    display_board(BOARD)
     game_logic()
 
 
