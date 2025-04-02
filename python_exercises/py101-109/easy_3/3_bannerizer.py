3da64c7b282b77a1be1a7c1903cb835b32bcfc45
Louie Mendez <lmendezjr024@gmail.com>
2025-04-02 17:22:02 -0400

    print in box complete

--- python_exercises/py101-109/easy_3/3_bannerizer.py
+++ python_exercises/py101-109/easy_3/3_bannerizer.py
@@ -1,14 +1,17 @@
 # Write a function that takes a short line of text and prints it within a box.
 
-def print_in_box(string):
-    dashes = '+'.rjust(len(string) + 3, '-')
-    print('+' + dashes)
-    print('|' + '|'.rjust(len(string) + 3))
-    print(f'| {string} |')
-    print('|' + '|'.rjust(len(string) + 3))
-    print('+' + dashes)
+def print_in_box(message, length = None):
+    if length is None:
+        length = len(message)
 
-print_in_box('To boldly go where no one has gone before.')
+    message = message[:length]
+    print(f'+-{'-' * len(message)}-+')
+    print(f'| {' ' * len(message)} |')
+    print(f'| {message} |')
+    print(f'| {' ' * len(message)} |')
+    print(f'+-{'-' * len(message)}-+')
+
+print_in_box('To boldly go where no one has gone before.', 10)
 # +--------------------------------------------+
 # |                                            |
 # | To boldly go where no one has gone before. |
