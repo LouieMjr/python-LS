1eb635f095c3400f22a52011cb2dc5abba43a0f3
Louie Mendez <lmendezjr024@gmail.com>
2025-04-07 16:01:00 -0400

    added prompt

--- python_exercises/py101-109/easy_3/double_doubles.py
+++ python_exercises/py101-109/easy_3/double_doubles.py
@@ -1,3 +1,10 @@
+# A double number is an even-length number whose left-side digits are exactly
+# the same as its right-side digits. For example, 44, 3333, 103103, and 7676 are
+# all double numbers, whereas 444, 334433, and 107 are not.
+#
+# Write a function that returns the number provided as an argument multiplied by
+# two, unless the argument is a double number. If the argument is a double
+# number, return the double number as-is.
 
 def twice(num):
     num_str = str(num)
