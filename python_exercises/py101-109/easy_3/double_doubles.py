022cd5eab15b71c12267ae5ee00097ccf7c62a7b
Louie Mendez <lmendezjr024@gmail.com>
2025-04-07 16:00:14 -0400

    double doubles complete

---
+++ python_exercises/py101-109/easy_3/double_doubles.py
@@ -0,0 +1,14 @@
+
+def twice(num):
+    num_str = str(num)
+    center = len(num_str) // 2
+    return num if num_str[:center] == num_str[center:] else num * 2
+
+print(twice(37) == 74)                  # True
+print(twice(44) == 44)                  # True
+print(twice(334433) == 668866)          # True
+print(twice(444) == 888)                # True
+print(twice(107) == 214)                # True
+print(twice(103103) == 103103)          # True
+print(twice(3333) == 3333)              # True
+print(twice(7676) == 7676)              # True
