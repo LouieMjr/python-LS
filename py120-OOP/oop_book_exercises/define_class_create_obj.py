ba387f1c253d46c0e0ad20e4af5ae6f369769125
Louie Mendez <lmendezjr024@gmail.com>
2025-04-29 21:16:13 -0400

    oop practice with class and instance methods

--- py120-OOP/oop_book_exercises/define_class_create_obj.py
+++ py120-OOP/oop_book_exercises/define_class_create_obj.py
@@ -6,8 +6,41 @@ class Person:
         print(f'Hi my name is {self.name} and i am a {self.type}')
 
 
-george = Person('george')
-print(george)
+# george = Person('george')
+# print(george)
 
-susan = Person('susan')
-print(susan)
+# susan = Person('susan')
+# print(susan)
+
+class Foo1:
+
+    @classmethod
+    def bar(cls):
+        print('this is bar in Foo1')
+
+    def qux(self):
+        print(self)
+        type(self).bar()
+        self.__class__.bar()
+        self.bar()
+        Foo1.bar()
+
+class Foo2(Foo1):
+
+    @classmethod
+    def bar(cls):
+        print('this is bar in Foo2')
+
+foo1 = Foo1()
+foo1.qux()
+# this is bar in Foo1
+# this is bar in Foo1
+# this is bar in Foo1
+# this is bar in Foo1
+
+foo2 = Foo2()
+foo2.qux()
+# this is bar in Foo2
+# this is bar in Foo2
+# this is bar in Foo2
+# this is bar in Foo1
