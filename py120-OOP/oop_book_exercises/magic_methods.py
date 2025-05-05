beaeb1038a33450f8743355d64fd85fbaa1330ca
Louie Mendez <lmendezjr024@gmail.com>
2025-05-05 15:52:57 -0400

    arithmetic methods

--- py120-OOP/oop_book_exercises/magic_methods.py
+++ py120-OOP/oop_book_exercises/magic_methods.py
@@ -16,3 +16,48 @@ class Car:
 vwbuzz = Car('ID.Buzz', 2024, 'red')
 print(vwbuzz)        # Red 2024 ID.Buzz
 print(repr(vwbuzz))  # Car('ID.Buzz', 2024, 'red')
+
+import math
+
+class Vector:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        if not isinstance(other, Vector):
+            return NotImplemented
+
+        new_x = self.x + other.x
+        new_y = self.y + other.y
+        return Vector(new_x, new_y)
+
+    # __iadd__ method omitted; we don't need it for this exercise
+
+    def __sub__(self, other):
+        new_x = self.x - other.x
+        new_y = self.y - other.y
+        return Vector(new_x, new_y)
+
+    def __mul__(self, other):
+        new_x = self.x * other.x
+        new_y = self.y * other.y
+        return new_x + new_y
+
+    def __abs__(self):
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def __repr__(self):
+        x = repr(self.x)
+        y = repr(self.y)
+        return f'Vector({x}, {y})'
+
+v1 = Vector(5, 12)
+v2 = Vector(13, -4)
+print(v1 + v2)      # Vector(18, 8)
+
+
+print(v1 - v2) # Vector(-8, 16)
+print(v1 * v2) # 17
+print(abs(v1)) # 13.0
