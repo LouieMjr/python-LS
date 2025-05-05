bfcfa25af97eb141d5ee9bb0bb27123437f25726
Louie Mendez <lmendezjr024@gmail.com>
2025-05-05 17:11:18 -0400

    magic methods practice

--- py120-OOP/oop_book_exercises/magic_methods.py
+++ py120-OOP/oop_book_exercises/magic_methods.py
@@ -53,11 +53,76 @@ class Vector:
         y = repr(self.y)
         return f'Vector({x}, {y})'
 
-v1 = Vector(5, 12)
-v2 = Vector(13, -4)
-print(v1 + v2)      # Vector(18, 8)
+# v1 = Vector(5, 12)
+# v2 = Vector(13, -4)
+# print(v1 + v2)      # Vector(18, 8)
+#
+#
+# print(v1 - v2) # Vector(-8, 16)
+# print(v1 * v2) # 17
+# print(abs(v1)) # 13.0
 
 
-print(v1 - v2) # Vector(-8, 16)
-print(v1 * v2) # 17
-print(abs(v1)) # 13.0
+
+class Candidate:
+    def __init__(self, name):
+        self.name = name
+        self.votes = 0
+
+    def __iadd__(self, vote):
+        self.votes += vote
+        return self
+
+
+
+class Election(Candidate):
+
+    def __init__(self, candidates):
+        self.candidates = candidates
+
+    def results(self):
+        tmp_max = 0
+        max_votes = 0
+        vote_count = 0
+        winner = None
+
+        for candidate in candidates:
+            vote_count = candidate.votes
+            if vote_count > tmp_max:
+                tmp_max = vote_count
+                winner = candidate.name
+
+            max_votes += vote_count
+            print(f'{candidate.name} : {vote_count} votes')
+
+        print(f'\n{winner} won: {(tmp_max / max_votes)*100:.1f}% of votes')
+
+
+mike_jones = Candidate('Mike Jones')
+susan_dore = Candidate('Susan Dore')
+kim_waters = Candidate('Kim Waters')
+
+print(mike_jones)
+
+candidates = {
+    mike_jones,
+    susan_dore,
+    kim_waters,
+}
+
+votes = [
+    mike_jones,
+    susan_dore,
+    mike_jones,
+    susan_dore,
+    susan_dore,
+    kim_waters,
+    susan_dore,
+    mike_jones,
+]
+
+for candidate in votes:
+    candidate += 1
+
+election = Election(candidates)
+election.results()
