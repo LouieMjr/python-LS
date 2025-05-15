fd30ca4903d6ce292e539995acc53fd76010b14f
Louie Mendez <lmendezjr024@gmail.com>
2025-05-15 15:32:01 -0400

    modified solution working with more test cases

--- py120-OOP/oop_easy_exercises/banner_class.py
+++ py120-OOP/oop_easy_exercises/banner_class.py
@@ -2,11 +2,6 @@ class Banner:
     def __init__(self, message, width=0):
         self.message = message
         self.width = width
-        if self.width > 9:
-            self.msg_len = width
-        else:
-            self.msg_len = len(message)
-
 
     def __str__(self):
         return "\n".join([self._horizontal_rule(),
@@ -15,49 +10,47 @@ class Banner:
                           self._empty_line(),
                           self._horizontal_rule()])
 
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, width):
+        if width > 9 and width < len(self.message):
+            self._width = width
+        else:
+            self._width = len(self.message)
+
     def _empty_line(self):
-        return f'| {' '*(self.msg_len)} |'
+        return f'| {' ' * self.width} |'
 
     def _horizontal_rule(self):
-        return f'+-{'-'*(self.msg_len)}-+'
+        return f'+-{'-' * self.width}-+'
 
     def _message_line(self):
-        if self.width < 10:
+        if self.width >= len(self.message):
             return f"| {self.message} |"
 
-        fit_msg = ''
-        msg_len = self.msg_len
-        leftover_msg = self.message
-        # num = len(self.message) // self.width + 1
-
-        chunks = ''
         chunk = ''
-        word_list = leftover_msg.split()
+        fit_msg = ''
 
-        for word in word_list:
+        msg_len = self.width
+        word_list = self.message.split()
+
+        for i, word in enumerate(word_list):
+
             if len(chunk + word) >= msg_len:
-                chunks += chunk
-                fit_msg += f"| {chunks.center(msg_len)} |\n"
-                chunks = ''
-                chunk = ' ' + word
+                fit_msg += f"| {chunk.center(msg_len)} |\n"
+                chunk = ''
+
+            # if its the last word in word_list
+            if i == len(word_list) - 1:
+                fit_msg += f"| {word.center(msg_len)} |"
 
             elif word != word_list[-1]:
                 chunk += ' ' + word
 
-            else:
-                chunks += chunk + ' ' + word
-                fit_msg += f"| {chunks.center(msg_len)} |"
-
         return fit_msg
-
-        # for i in range(num):
-        #     if i == range(num)[-1]:
-        #         fit_msg += f"| {leftover_msg[:msg_len].center(msg_len)} |"
-        #         return fit_msg
-        #
-        #     fit_msg += f"| {leftover_msg[:self.msg_len]} |\n"
-        #     leftover_msg = leftover_msg[self.msg_len:]
-
 
 # Complete this class so that the test cases shown below work as intended. You are free to add any methods or instance variables you need. However, methods prefixed with an underscore are intended for internal use and should not be called externally.
 #
@@ -65,7 +58,10 @@ class Banner:
 
 # Comments show expected output
 
-banner = Banner('To boldly go where no one has gone before.', 22)
+banner = Banner('To boldly go where no one has gone before.', 40)
+print(banner)
+
+banner = Banner('To boldly go where no one has gone before.')
 print(banner)
 # +--------------------------------------------+
 # |                                            |
